6791629198c73ef0b054fb7adc5b3c9ee3e594ce
thomasrea0113 <thomasrea0113@gmail.com>
2021-06-29 15:25:35 -0400

    use puppeteer cluster

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
+const { Cluster } = require('puppeteer-cluster');
+
 const puppeteer = require('puppeteer-extra');
 
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
 (async () => {
-    const browser = await puppeteer.launch({
-        // headless: true,
-        args: [
-            // TODO apparently, this is a bad idea. Should configure the docker container to use a different UID,
-            // because when running as root, these arguments are required
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
+    const cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 4,
+        puppeteer,
+        puppeteerOptions: {
+            args: [
+                // TODO apparently, this is a bad idea. Should configure the docker container to use a different UID,
+                // because when running as root, these arguments are required
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
 
-            '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"'],
+                '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"'
+            ]
+        }
     });
-    const page = await browser.newPage();
-    await page.goto(process.argv[2])
-    const content = await page.content();
-    await browser.close()
-    console.log(content);
+
+    const contents = {};
+
+    await cluster.task(async ({ page, data: url }) => {
+        await page.goto(url);
+        contents[url] = await page.content();
+    });
+
+    process.argv.slice(2).forEach(u => cluster.queue(u));
+
+    await cluster.idle();
+    await cluster.close();
+
+    console.log(contents)
 })();
